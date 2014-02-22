fd5e1029e371a6d6d44d07101f0d5e0426231293
Karl Gustav Røksund <kkgg84+git@gmail.com>
2014-02-22 17:50:58 +0100

    7. bowling test, succeeding

--- app/js/services.js
+++ app/js/services.js
@@ -13,7 +13,13 @@ mod.service('bowling', function(){
 	var sets = []
 	this.roll = function(setArray){
 		if( ! angular.isArray(setArray) ) throw new Error("Illegal argument, the set can only be an array!")
-		if(setArray.length != 2) throw new Error()
+		if(sets.length == 9){
+			if( ! (setArray.length == 2 || setArray.length == 3)){
+				throw new Error("You are only alowed to have 3 balls in a set if it's the 10. set!")
+			}
+		} else {
+			if(setArray.length != 2) throw new Error()
+		}
 
 		sets.push(setArray)
 
