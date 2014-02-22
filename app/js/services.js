c18267e51908b9ac1481fea7f71f55c340c5e4c6
Karl Gustav Røksund <kkgg84+git@gmail.com>
2014-02-22 17:22:41 +0100

    4. bowling test, succeeding

--- app/js/services.js
+++ app/js/services.js
@@ -10,7 +10,11 @@ var mod = angular.module('myApp.services', [])
 mod.value('version', '0.1');
 
 mod.service('bowling', function(){
+	var sets = []
 	this.roll = function(setArray){
-		return setArray[0] + setArray[1]
+		sets.push(setArray)
+		return sets.sum(function(aSet){
+			return aSet.sum()
+		})
 	}
 })
