1b6a5c225de41373d6cecd7ca7808d323a884bf3
Karl Gustav Røksund <kkgg84+git@gmail.com>
2014-02-22 17:38:43 +0100

    6. bowling test, succeeding

--- app/js/services.js
+++ app/js/services.js
@@ -12,6 +12,7 @@ mod.value('version', '0.1');
 mod.service('bowling', function(){
 	var sets = []
 	this.roll = function(setArray){
+		if( ! angular.isArray(setArray) ) throw new Error("Illegal argument, the set can only be an array!")
 		if(setArray.length != 2) throw new Error()
 
 		sets.push(setArray)
